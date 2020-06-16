b22f57ab2a102aea6e69f65ccb0bc72fd458e2d1
JuliaBergman <juliabergman523@gmail.com>
2020-06-15 22:04:42 -0300

    lição em andamento

---
+++ AvaliaDesktop_Parte_1_JuliaBergman_2T/View/FormCadastro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AvaliaDesktop_Parte_1_JuliaBergman_2T.View
+{
+    public partial class FormCadastro : Form
+    {
+        public FormCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
